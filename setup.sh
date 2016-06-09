45d027490673c5303cc78a4147bed24fde262fb3
clegganator259 <djclegg01@hotmail.com>
2016-06-09 14:32:24 +0100

    Fixing scripts

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
+BASEDIR=$(cd "$(dirname $0)" && pwd)
 pacman -S i3 --noconfirm
 pacman -S dmenu --noconfirm
 pacman -S vim --noconfirm
-ln -nsf i3 $1/.config
-ln -nsf vim/.vimrc $1/.vimrc
-ln -nsf Xconfig/.Xresources $1/.Xresources
-ln -nsf Xconfig/.xprofile $1/.xprofile
+ln -nsf $BASEDIR/i3 $1/.config
+ln -nsf $BASEDIR/vim/.vimrc $1/.vimrc
+ln -nsf $BASEDIR/Xconfig/.Xresources $1/.Xresources
+ln -nsf $BASEDIR/Xconfig/.xprofile $1/.xprofile
+ln -nsf $BASEDIR/urxvt/clipboard /usr/lib/urxvt/perl/clipboard
