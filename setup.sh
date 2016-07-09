e603940c3ed5e4b78338a164fcaacc376b6cd15d
dxc <djclegg01@hotmail.com>
2016-07-09 16:10:29 +0100

    Installs urxvt

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ BASEDIR=$(cd "$(dirname $0)" && pwd)
 pacman -S i3 --noconfirm
 pacman -S dmenu --noconfirm
 pacman -S vim --noconfirm
+pacman -S rxvt-unicode --noconfirm
 ln -nsf $BASEDIR/i3 $1/.config
 ln -nsf $BASEDIR/vim/.vimrc $1/.vimrc
 ln -nsf $BASEDIR/Xconfig/.Xresources $1/.Xresources
