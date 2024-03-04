4d5c606fda598d9a7e1d5cd448e26d0a46f44cec
David Clegg <djclegg01@hotmail.com>
2024-03-04 11:27:16 +0100

    Feat: Adding default arg to setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 BASEDIR=$(cd "$(dirname $0)" && pwd)
+TARGET_DIR=${1:-~}
+# Running example
 #Installs stuff
-ln -nsf $BASEDIR/bash/.bashrc $1/.bashrc
-ln -nsf $BASEDIR/bash/git-prompt.sh $1/git-prompt.sh
-ln -nsf $BASEDIR/tmux/.tmux.conf $1/.tmux.conf
-ln -nsf $BASEDIR/alacritty/alacritty.yml $1/.config/alacritty/alacritty.yml
+ln -nsf $BASEDIR/bash/.bashrc $TARGET_DIR/.bashrc
+ln -nsf $BASEDIR/bash/git-prompt.sh $TARGET_DIR/git-prompt.sh
+ln -nsf $BASEDIR/tmux/.tmux.conf $TARGET_DIR/.tmux.conf
+ln -nsf $BASEDIR/alacritty/alacritty.yml $TARGET_DIR/.config/alacritty/alacritty.yml
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
