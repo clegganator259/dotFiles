064a00b778db614cec184c1420bbd2e664bce672
david.clegg <djclegg01@hotmail.com>
2024-03-11 12:41:37 +0100

    Feat: Include alacritty config in setup script

--- setup.sh
+++ setup.sh
@@ -17,5 +17,5 @@ TARGET_DIR=${1:-~}
 ln -nsf $BASEDIR/bash/.bashrc $TARGET_DIR/.bashrc
 ln -nsf $BASEDIR/bash/git-prompt.sh $TARGET_DIR/git-prompt.sh
 ln -nsf $BASEDIR/tmux/.tmux.conf $TARGET_DIR/.tmux.conf
-ln -nsf $BASEDIR/alacritty/alacritty.yml $TARGET_DIR/.config/alacritty/alacritty.yml
+ln -nsf $BASEDIR/alacritty/alacritty.toml $TARGET_DIR/.config/alacritty/alacritty.toml
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
