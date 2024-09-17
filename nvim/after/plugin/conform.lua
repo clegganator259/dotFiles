b86765349196e5e8405ccc5a0fc3b9eb6292d1d0
David Clegg <david.clegg@yubico.com>
2024-09-17 23:22:19 +0200

    Feat: Updating vim config to work with new packages

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -5,11 +5,8 @@ require("conform").setup({
 		python = { "isort", "black" },
 	},
 	lsp_fallback = true,
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf, timeout_ms = 2000 })
-	end,
+	format_on_save = {
+		timeout_ms = 2500,
+		lsp_fallback = true,
+	},
 })
